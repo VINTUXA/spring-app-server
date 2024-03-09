e8c855ca4b5c1691864aa6dde6a8de38c2d70a0b
VINTUXA <dvo.performance@gmail.com>
2024-03-09 13:08:54 +0300

    inti

---
+++ src/test/java/com/example/springappserver/SpringAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
